cdefba1850f80bc331353f3e59fb3b79b4cdc205
mandadhikalyan6@gmail.com <mandadhikalyan6@gmail.com>
2026-08-25 16:10:23 +0530

    Initial commit

---
+++ angular-frontend/src/app/employee-list/employee-list.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee-service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [NgForOf],
+  templateUrl: './employee-list.html',
+  styleUrl: './employee-list.css',
+})
+export class EmployeeList {
+  employees: Employee[] = [];
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router,
+  ) {}
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  private getEmployees() {
+    this.employeeService.getEmployeeList().subscribe((data) => {
+      this.employees = data;
+    });
+  }
+  employeeDetails(id: number) {
+    this.router.navigate(['employee-details', id]);
+  }
+  updateEmployee(id: number) {
+    this.router.navigate(['update-employee', id]);
+  }
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id).subscribe((data) => {
+      console.log(data);
+      this.getEmployees();
+    });
+  }
+}
